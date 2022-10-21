dab26cd45e281a347260554c9ebc4245e44cbe2f
apisceva <aljona.gromova@gmail.com>
2022-10-21 05:56:17 -0400

    added api with geoApiOptions and GEO_API_URL

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "04f6542d09mshd7dff013d76d1abp15bf1cjsnb5d19ba2bb60",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
