ebaa7039854dc4fe920461e545a815041a8338de
apisceva <aljona.gromova@gmail.com>
2022-10-25 06:52:54 -0400

    some changes in search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -15,7 +15,7 @@ const Search = ({ onSearchChange }) => {
         return {
           options: response.data.map((city) => {
             return {
-              value: `${city.latitude} ${city.latitude}`,
+              value: `${city.latitude} ${city.longitude}`,
               label: `${city.name}, ${city.countryCode}`,
             };
           }),
